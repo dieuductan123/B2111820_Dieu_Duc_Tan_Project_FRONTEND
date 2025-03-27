264a3004ef8eb9806c577347eb75965423be1850
tandieuduc <ductandieu456@gmail.com>
2025-03-27 14:22:09 +0700

    Khoi tao Frontend cho Ung dung Quan ly muon sach Online

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+
+}
+</script>
+
+<template>
+  <h1>Good Afternoon, this is my application</h1>
+</template>
+
+<style>
+.page {
+  max-width: 400px;
+  margin: auto;
+}
+</style>
