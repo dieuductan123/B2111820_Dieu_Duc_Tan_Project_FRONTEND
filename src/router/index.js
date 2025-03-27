e7ae65bb7403d47aa7ac2aa32231d1652791be31
tandieuduc <ductandieu456@gmail.com>
2025-03-27 14:36:00 +0700

    Thiet ke trang NotFound cho ung dung

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,13 @@
 import { createWebHistory, createRouter } from "vue-router";
 
+import NotFound from '@/views/NotFound.vue';
 const routes = [
-
-]
+  {
+    path: "/:pathMatch(.*)*", // Sửa lại `patchMatch` thành `pathMatch`
+    name: "not_found",
+    component: NotFound,
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
---
+++ src/views/NotFound.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="d-flex justify-content-center align-items-center vh-100">
+    <div>
+      <h1 class="text-center">
+        KHÔNG TÌM THẤY TRANG WEB BẠN YÊU CẦU
+      </h1>
+      <router-link to="/">
+        <p class="text-center more-info text-underline">Trở về trang chủ</p>
+      </router-link>
+    </div>
+    <h1></h1>
+  </div>
+</template>
+
+<script></script>
+<style scoped>
+h1 {
+  font-weight: bolder;
+}
+
+.text-underline {
+  text-decoration: underline;
+}
+</style>
