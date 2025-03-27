18664f57352b79d616a20f2016d3439c812c2bc5
tandieuduc <ductandieu456@gmail.com>
2025-03-27 14:56:02 +0700

    Thiet ke trang Yeu cau nguoi dung Dang nhap

---
+++ src/views/UnLogin.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="d-flex justify-content-center align-items-center vh-100">
+    <div>
+      <h1 class="text-center">
+        BẠN CẦN ĐĂNG NHẬP ĐỂ SỬ DỤNG CÁC CHỨC NĂNG CỦA HỆ THỐNG
+      </h1>
+      <router-link to="/login">
+        <p class="text-center more-info text-underline">Đăng nhập ngay</p>
+      </router-link>
+    </div>
+    <h1></h1>
+  </div>
+</template>
+
+<script></script>
+<style scoped>
+h1 {
+  font-weight: bolder;
+}
+
+.text-underline {
+  text-decoration: underline;
+}
+</style>
