0567ec51b0009dbb1f64871da50b1b8662a7ba99
tandieuduc <ductandieu456@gmail.com>
2025-03-27 14:31:56 +0700

    Cai dat bootstrap5, fontawesome va vue-router4 cho ung dung

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
-import './assets/main.css'
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "@popperjs/core";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+import router from "@/router";
+
+createApp(App).use(router).mount("#app");
---
+++ src/router/index.js
@@ -0,0 +1,12 @@
+import { createWebHistory, createRouter } from "vue-router";
+
+const routes = [
+
+]
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes,
+});
+
+export default router;
